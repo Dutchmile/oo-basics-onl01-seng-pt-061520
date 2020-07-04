ca8c6d310df4b43cb7659f97004a8d2629adec88
Christopher Dutcher <cadutcher@icloud.com>
2020-07-04 13:16:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,7 +11,6 @@ class Book
   def author=(author)
     @author = author
   end
-
   def author
     @author
   end
@@ -19,8 +18,14 @@ class Book
   def page_count=(num)
     @page_count = num
   end
-
   def page_count
     @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
   end
 end
